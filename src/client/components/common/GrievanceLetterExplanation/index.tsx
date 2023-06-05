f650c0501cb3469cd51f9a0730c97a693331b394
DEEPESH S <deepesh@madon.io>
2023-06-05 13:34:42 +0530

    feat: added letter preview to grievance letter explanation

--- src/client/components/common/GrievanceLetterExplanation/index.tsx
+++ src/client/components/common/GrievanceLetterExplanation/index.tsx
@@ -3,6 +3,7 @@ import { Typography } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import EndToEndStepper from '../EndToEndStepper';
 import ActionBar from '../ActionBar';
+import VLcard from '../VLcard';
 
 const GrievanceLetterExplanation: React.FC = () => {
 	const history = useHistory();
@@ -14,7 +15,7 @@ const GrievanceLetterExplanation: React.FC = () => {
 	return (
 		<div className="flex-col w-full grievance-explanation">
 			<EndToEndStepper step={3} />
-			<div className="step-intro">
+			<div className="letter-preview">
 				<h2>Grievance appeals</h2>
 				<Typography variant="body1" paragraph>
 					If you have lodged a grievance and received a response from your employer rejecting your complaint, then you
@@ -56,6 +57,46 @@ const GrievanceLetterExplanation: React.FC = () => {
 					do not ask for ACAS to issue the Certificate straight away) because this provides additional time for us to
 					see if we can help you resolve your workplace issue without further legal action.
 				</Typography>
+				<div className="my-5 py-5">
+					<VLcard heading="Grievance appeal" theme="light">
+						<div className="letter-preview__body">
+							<div className="grievance-appeal-preview">PRIVATE & CONFIDENTIAL</div>
+							<div className="grievance-appeal-preview">[HR manager]</div>
+							<div className="grievance-appeal-preview">[Company name]</div>
+							<div className="grievance-appeal-preview">By email only to: [Email]</div>
+							<div className="grievance-appeal-preview">[Today]</div>
+							<div className="grievance-appeal-preview">Dear [HR manager]</div>
+							<div className="grievance-appeal-preview">I write to appeal the outcome of my grievance.</div>
+							<div className="grievance-appeal-preview">##Witnesses not interviewed</div>
+							<div className="grievance-appeal-preview">
+								The following witnesses were not interviewed despite being mentioned in my grievance: [names]. You
+								should now interview these witnesses as part of this appeal.
+							</div>
+							<div className="grievance-appeal-preview">##Documentary evidence not considered</div>
+							<div className="grievance-appeal-preview">
+								The following documentary evidence was not considered, despite being mentioned in my grievance: [list
+								items, for example: Email of [date] from me to [name] about [topic]]. You should now consider these
+								documents as part of this appeal.
+							</div>
+							<div className="grievance-appeal-preview">##Didn’t take account of points raised</div>
+							<div className="grievance-appeal-preview">
+								The grievance outcome didn’t take into account the following points raised in my grievance: [list the
+								points which weren’t considered].
+							</div>
+							<div className="grievance-appeal-preview">##Outcome unfair in all the circumstances</div>
+							<div className="grievance-appeal-preview">
+								The grievance outcome was unfair in all the circumstances, especially: [list other reasons why it was
+								unfair, for example that your line manager was deciding the grievance, despite the grievance being about
+								them].
+							</div>
+							<div className="grievance-appeal-preview">
+								I look forward to hearing from you at your earliest convenience.
+							</div>
+							<div className="grievance-appeal-preview">Yours sincerely</div>
+							<div className="grievance-appeal-preview">[name]</div>
+						</div>
+					</VLcard>
+				</div>
 				<ActionBar step={2} nextHandler={handleNext} />
 			</div>
 		</div>
